5aae953ba337c6b03239f1c856e0b34287f0115e
刘程鑫 <756004757@qq.com>
2025-02-09 23:41:16 +0800

    fix: login

--- src/lib/abi.ts
+++ src/lib/abi.ts
@@ -776,7 +776,7 @@ export const payAbi = [
 				"type": "function"
 			}
 ]
-		
+
 export const payAddress = "0xd930Ec735612988B820368E9fb6Ef26759777F0C"			//erc20合约	
 
-export const transferAddress = "0xf6771010F62E1f5EE3A54fd39db72c5C6C5ccf4" //中转合约
+export const transferAddress = "0xf6771010F62E1f5EE3A54fd39db72c5C6C5ccf45" //中转合约
