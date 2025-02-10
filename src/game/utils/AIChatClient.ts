1deacc7b390d633049c4b7e742303289edfc1771
donnykk <donnykk9527@gmail.com>
2025-02-10 20:41:15 +0800

    fix: ai agent port mix content error

--- src/game/utils/AIChatClient.ts
+++ src/game/utils/AIChatClient.ts
@@ -6,7 +6,7 @@ class AIChatClient {
     // Use proxy URL in development
     private API_URL = process.env.NODE_ENV === 'development'
         ? '/api/chat'  // This will be proxied
-        : "httpo://43.134.74.254:8080/api/chat";
+        : "http://43.134.74.254:8080/api/chat";
 
     private constructor() { }
 
