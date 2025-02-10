1b173638e598bf64c7581a65837a63956b908a1a
donnykk <donnykk9527@gmail.com>
2025-02-10 19:56:43 +0800

    update:new backend port

--- src/game/utils/ColyseusClient.ts
+++ src/game/utils/ColyseusClient.ts
@@ -1,6 +1,7 @@
 import { Client, Room } from "colyseus.js";
 
-const COLYSEUS_SERVER = "https://build-don-url-ste.trycloudflare.com  ";
+//const COLYSEUS_SERVER = "https://build-don-url-ste.trycloudflare.com  ";
+const COLYSEUS_SERVER = "ws://13.212.89.194:2568"
 //const COLYSEUS_SERVER = "ws://47.236.128.7:2568";
 //const COLYSEUS_SERVER = "ws://localhost:2568";
 
